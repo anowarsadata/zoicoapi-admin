f93a3b5d29ec95ec7a35e71543009113ec811a3c
Anowar Sadat <anowar.vista@gmail.com>
2025-06-09 18:37:26 +0530

    Admin update for user page

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,49 +3,22 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Form;
+use Spatie\Permission\Models\Role;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
 
-    public function form(Form $form): Form
-    {
-        $statusOptions = [
-            0 => 'In-active',
-            1 => 'Active',
-        ];
-
-        return $form
-            ->schema([
-                TextInput::make('name')->required(),
-                TextInput::make('email')->required()->email()->unique(),
-                TextInput::make('password')->required()->password(),
-                Select::make('roles')
-                    ->label('User Role')
-                    ->relationship('roles', 'name')
-                    ->searchable()
-                    ->preload()
-                    ->required()
-                    ->native(false),
-                Select::make('status')
-                    ->label('Status')
-                    ->options($statusOptions)
-                    ->required()
-                    ->native(false),
-            ]);
-    }
+    protected $roleId;
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // Prevent trying to save 'roles' directly to the users table
+        // Store role ID and remove from form data
+        $this->roleId = $data['roles'];
         unset($data['roles']);
 
-        // Encrypt password before save
+        // Hash password
         $data['password'] = bcrypt($data['password']);
 
         return $data;
@@ -53,6 +26,12 @@ class CreateUser extends CreateRecord
 
     protected function afterCreate(): void
     {
-        $this->record->syncRoles($this->form->getState()['roles']);
+        $role = Role::findById($this->roleId, 'web'); // Get role name by ID
+        $this->record->syncRoles($role->name);        // Use role name, not ID
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return UserResource::getUrl('index');
     }
 }
